456c23bd3a57d0bda559f84bded45c3c19ad354d
ThomasGronvold <thomas.groenvold@gmail.com>
2023-08-31 09:43:17 +0200

    Some changes
    Testing if this fixes things for Heroku

--- app.js
+++ app.js
@@ -82,10 +82,9 @@ app.post("/delete", (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
-// app.listen(port);
-
-app.listen(port, function () {
-  console.log("Server started on port 3000");
-});
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});   
